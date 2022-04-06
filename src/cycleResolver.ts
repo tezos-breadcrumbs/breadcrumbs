2c82aa259079af08c8e7e33aed546b7160f39573
kalouo <omar@zipmex.com>
2022-04-06 10:40:33 +0200

    refator: cycleResolver

--- src/cycleResolver.ts
+++ src/cycleResolver.ts
@@ -1,12 +1,27 @@
 import axios from "axios";
+import _ from "lodash";
 
-export const getLastCycle = async (): Promise<Number | void> => {
+import { Payment } from "../database/models";
+
+const getLastCycle = async (): Promise<Number | void> => {
   return axios
     .get("https://api.tzkt.io/v1/head")
     .then(({ data: { cycle: headCycle } }) => headCycle - 1)
     .catch(console.error);
 };
 
-const getLastPaidCycle = () => {
-  /* Fetch latest cycle from Postgres or null*/
+const getLastProcessedCycle = async () => {
+  const result = await Payment.findAll({
+    order: [["cycle", "DESC"]],
+    limit: 1,
+  });
+
+  return _.isEmpty(result) ? null : result[0].cycle;
+};
+
+export const getStartingCycle = async (): Promise<Number> => {
+  const lastCycle = await getLastCycle();
+  const lastProcessedCycle = await getLastProcessedCycle();
+
+  return lastProcessedCycle ? lastProcessedCycle : lastCycle;
 };
