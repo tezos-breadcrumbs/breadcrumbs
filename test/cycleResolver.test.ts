ea08a448d28b38c6b32bda7f4dc6eaabc8289f87
kalouo <omar@zipmex.com>
2022-04-06 10:41:22 +0200

    test: cycleResolver (WIP)

---
+++ test/cycleResolver.test.ts
@@ -0,0 +1,27 @@
+/** @jest-environment setup-polly-jest/jest-environment-node */
+
+import axios from "axios";
+import { getStartingCycle } from "../src/cycleResolver";
+import * as Polly from "./polly";
+
+describe("test", () => {
+  let db: any = require("../database/models");
+  Polly.start();
+
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
+
+  test("returns last concluded cycle if the database is empty", async () => {
+    const actual = await getStartingCycle();
+    const expected = await axios
+      .get("https://api.tzkt.io/v1/head")
+      .then(({ data: { cycle: headCycle } }) => headCycle - 1);
+
+    expect(actual).toEqual(expected);
+  });
+
+  afterAll(async () => {
+    await db.sequelize.close();
+  });
+});
