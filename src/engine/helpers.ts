28a3af753730718c9489d49cf954a54e0a795221
kalouo <omar@zipmex.com>
2022-04-22 19:34:34 +0200

    add: initializeCycleReport

--- src/engine/helpers.ts
+++ src/engine/helpers.ts
@@ -1,5 +1,6 @@
 import BigNumber from "bignumber.js";
 import { Config } from "src/config/interface";
+import { CycleReport } from "./interfaces";
 
 export const getApplicableFee = (config: Config, delegator: string) => {
   return new BigNumber(config.fee_exceptions[delegator] || config.default_fee);
@@ -15,4 +16,13 @@ export const isOverDelegated = (
 ): boolean => {
   const TEN_PERCENT = new BigNumber(0.1);
   return bakerBalance.div(totalStake).lt(TEN_PERCENT);
+};
+
+export const initializeCycleReport = (cycle): CycleReport => {
+  return {
+    cycle,
+    payments: [],
+    lockedBondRewards: new BigNumber(0),
+    feeIncome: new BigNumber(0),
+  };
 };
