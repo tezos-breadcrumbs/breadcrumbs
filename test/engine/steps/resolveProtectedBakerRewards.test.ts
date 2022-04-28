fbd602e181616df55be876c2eb88764814430171
kalouo <omar@zipmex.com>
2022-04-28 22:08:46 +0100

    test: resolveProtectedBakerRewards

--- test/engine/steps/resolveProtectedBakerRewards.test.ts
+++ test/engine/steps/resolveProtectedBakerRewards.test.ts
@@ -13,7 +13,7 @@ import * as Polly from "test/helpers/polly";
 describe("resolveProtectedBakerRewards", () => {
   Polly.start();
 
-  test("adds baker stake to `cycleShares` for processing if baker is not overdelegated", async () => {
+  test("adds baker stake to `cycleShares` for regular processing if not overdelegated", async () => {
     const config = generateConfig({ overdelegation_guard: true });
     const cycleData = await client.getCycleData(config.baking_address, 470);
     const {
@@ -51,7 +51,7 @@ describe("resolveProtectedBakerRewards", () => {
     expect(actual).toStrictEqual(expected);
   });
 
-  test("adds baker stake to `cycleShares` for processing if baker is overdelegated and `overdelegation_guard` is false", async () => {
+  test("adds baker stake to `cycleShares` for regular processing if overdelegated and `overdelegation_guard` is false", async () => {
     const config = generateConfig({
       baking_address: "tz1cZfFQpcYhwDp7y1njZXDsZqCrn2NqmVof" /* Tezos Rio */,
       overdelegation_guard: false,
@@ -92,5 +92,102 @@ describe("resolveProtectedBakerRewards", () => {
     );
 
     expect(actual).toStrictEqual(expected);
+  });
+
+  test("allocates 10% of rewards to baker if overdelegated and `overdelegation_guard` is true  (case: frozen deposit limit)", async () => {
+    const config = generateConfig({
+      baking_address: "tz1cZfFQpcYhwDp7y1njZXDsZqCrn2NqmVof" /* Tezos Rio */,
+      overdelegation_guard: true,
+    });
+
+    const cycleData = await client.getCycleData(config.baking_address, 475);
+    const {
+      cycleStakingBalance,
+      cycleDelegatedBalance,
+      frozenDepositLimit,
+      cycleRewards,
+    } = cycleData;
+
+    const bakerBalance = subtract(cycleStakingBalance, cycleDelegatedBalance);
+
+    /* Tezos Rio is overdelegated based on a frozen deposit limit in cycle 475 */
+    expect(
+      isOverDelegated(bakerBalance, cycleStakingBalance, frozenDepositLimit)
+    ).toBe(true);
+
+    const args = {
+      config,
+      cycleData,
+      cycleReport: initializeCycleReport(475),
+      distributableRewards: cycleData.cycleRewards,
+    };
+
+    const actual = resolveProtectedBakerRewards(args);
+
+    const expected = {
+      ...args,
+      cycleReport: {
+        ...args.cycleReport,
+        lockedBondRewards: cycleRewards.div(10),
+      },
+      distributableRewards: cycleRewards.minus(cycleRewards.div(10)),
+    };
+
+    expect(actual).toStrictEqual(expected);
+
+    const bakerIncludedInShares = _.some(actual.cycleData.cycleShares, {
+      address: config.baking_address,
+    });
+
+    expect(bakerIncludedInShares).toBe(false);
+  });
+
+  test("allocates 10% of rewards to baker if overdelegated and `overdelegation_guard` is true  (case: actual stake)", async () => {
+    const config = generateConfig({
+      baking_address:
+        "tz1axcnVN9tZnCe4sQQhC6f3tfSEXdjPaXPY" /* Devil's Delegate */,
+      overdelegation_guard: true,
+    });
+
+    const cycleData = await client.getCycleData(config.baking_address, 476);
+    const {
+      cycleStakingBalance,
+      cycleDelegatedBalance,
+      frozenDepositLimit,
+      cycleRewards,
+    } = cycleData;
+
+    const bakerBalance = subtract(cycleStakingBalance, cycleDelegatedBalance);
+
+    /* Devil's Delegated is overdelegated based on actual bond in cycle 476 */
+    expect(
+      isOverDelegated(bakerBalance, cycleStakingBalance, frozenDepositLimit)
+    ).toBe(true);
+
+    const args = {
+      config,
+      cycleData,
+      cycleReport: initializeCycleReport(476),
+      distributableRewards: cycleData.cycleRewards,
+    };
+
+    const actual = resolveProtectedBakerRewards(args);
+
+    const expected = {
+      ...args,
+      cycleReport: {
+        ...args.cycleReport,
+        lockedBondRewards: cycleRewards.div(10),
+      },
+      distributableRewards: cycleRewards.minus(cycleRewards.div(10)),
+    };
+
+    expect(actual).toStrictEqual(expected);
+
+    const bakerIncludedInShares = _.some(actual.cycleData.cycleShares, {
+      address: config.baking_address,
+    });
+
+    expect(bakerIncludedInShares).toBe(false);
   });
 });
