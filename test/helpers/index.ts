34019cd2327ef7cc971171f1130ff2f88bec1554
kalouo <omar@zipmex.com>
2022-05-13 14:09:35 +0200

    enrich generateConfig test helper

--- test/helpers/index.ts
+++ test/helpers/index.ts
@@ -7,13 +7,16 @@ export const generateConfig = (
     baking_address?: string;
     overdelegation_guard?: boolean;
     overdelegation_blacklist?: string[];
+    redirect_payments?: { [key: string]: string };
+    fee_exceptions?: { [key: string]: string };
+    default_fee?: string;
   } = {}
 ): Config => {
   return {
     baking_address: args.baking_address || DEFAULT_BAKER,
-    default_fee: "5",
-    redirect_payments: {},
-    fee_exceptions: {},
+    default_fee: args.default_fee || "5",
+    redirect_payments: args.redirect_payments || {},
+    fee_exceptions: args.fee_exceptions || {},
     overdelegation_guard: args.overdelegation_guard || false,
     overdelegation_blacklist: args.overdelegation_blacklist || [],
   };
