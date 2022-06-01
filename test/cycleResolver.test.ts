df3477375b0f09e2b8fa12b6a257e8a204038632
kalouo <omar@zipmex.com>
2022-06-01 18:19:12 +0700

    fix

--- test/cycleResolver.test.ts
+++ test/cycleResolver.test.ts
@@ -7,27 +7,27 @@ import { payments_1 } from "./helpers/data";
 
 import * as Polly from "./helpers/polly";
 
-describe("test", () => {
-  // let db: any = require("../database/models");
-  // const { Payment } = db;
-  // Polly.start();
-  // beforeAll(async () => {
-  //   await db.sequelize.sync({ force: true });
-  // });
-  // test("returns network's last concluded cycle if the database is empty", async () => {
-  //   const actual = await getStartingCycle();
-  //   const expected = await axios
-  //     .get("https://api.tzkt.io/v1/head")
-  //     .then(({ data: { cycle: headCycle } }) => headCycle - 1);
-  //   expect(actual).toEqual(expected);
-  // });
-  // test("returns cycle after the last processed one if database is not empty", async () => {
-  //   await Payment.bulkCreate(payments_1);
-  //   const actual = await getStartingCycle();
-  //   const expected = _.orderBy(payments_1, ["cycle"], ["desc"])[0]["cycle"];
-  //   expect(actual).toEqual(expected);
-  // });
-  // afterAll(async () => {
-  //   await db.sequelize.close();
-  // });
-});
+// describe("test", () => {
+// let db: any = require("../database/models");
+// const { Payment } = db;
+// Polly.start();
+// beforeAll(async () => {
+//   await db.sequelize.sync({ force: true });
+// });
+// test("returns network's last concluded cycle if the database is empty", async () => {
+//   const actual = await getStartingCycle();
+//   const expected = await axios
+//     .get("https://api.tzkt.io/v1/head")
+//     .then(({ data: { cycle: headCycle } }) => headCycle - 1);
+//   expect(actual).toEqual(expected);
+// });
+// test("returns cycle after the last processed one if database is not empty", async () => {
+//   await Payment.bulkCreate(payments_1);
+//   const actual = await getStartingCycle();
+//   const expected = _.orderBy(payments_1, ["cycle"], ["desc"])[0]["cycle"];
+//   expect(actual).toEqual(expected);
+// });
+// afterAll(async () => {
+//   await db.sequelize.close();
+// });
+// });
