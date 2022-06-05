3057a7ccd6ef741daf507845d49c3f4841765848
cryi <cryi@tutanota.com>
2022-06-05 08:53:44 +0200

    added missing files

---
+++ src/cli/cmd/pay.ts
@@ -0,0 +1,42 @@
+import { initializeCycleReport } from "src/engine/helpers";
+
+import client from "src/api-client";
+import engine from "src/engine";
+import { get_config } from "src/config";
+import { printPaymentsTable } from "src/cli/print";
+import { createProvider, prepareTransaction, submitBatch } from "src/tezos-client";
+import { arePaymentsRequirementsMet } from "src/engine/validate";
+import { cliOptions } from "src/cli";
+
+
+export const pay = async (cycle: number) => {
+	if (cliOptions.dryRun) {
+		console.log(`Running in 'dry-run' mode...`)
+	}
+
+	const config = get_config()
+
+	const cycleReport = initializeCycleReport(cycle);
+	const cycleData = await client.getCycleData(config.baking_address, cycle);
+
+	const result = engine.run({
+		config,
+		cycleReport,
+		cycleData,
+		distributableRewards: cycleData.cycleRewards,
+	});
+
+	const { delegatorPayments, feeIncomePayments, bondRewardPayments } =
+		result.cycleReport;
+
+	const allPayments = [...delegatorPayments, ...feeIncomePayments, ...bondRewardPayments];
+	const transactions = allPayments.filter(arePaymentsRequirementsMet).map(prepareTransaction)
+
+	if (cliOptions.dryRun) {
+		printPaymentsTable(allPayments)
+		process.exit(0)
+	}
+
+	const provider = createProvider();
+	await submitBatch(provider, transactions);
+}
---
+++ src/cli/validate.ts
@@ -0,0 +1,9 @@
+import { parseInt } from "lodash";
+
+export const validateCycleArg = (value) => {
+	const cycle = parseInt(value);
+	if (isNaN(cycle)) {
+		throw Error("No cycle number given.");
+	}
+	return cycle
+}
