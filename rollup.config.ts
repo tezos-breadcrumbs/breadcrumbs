88e1a4f263cad0691372bb64bcfe3508e8196918
cryi <cryi@tutanota.com>
2022-06-12 12:23:26 +0200

    bundling & pkg

---
+++ rollup.config.ts
@@ -0,0 +1,36 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+// import { createBasicConfig } from '@open-wc/building-rollup';
+
+// const baseConfig = createBasicConfig();
+// console.log(baseConfig)
+
+export default {
+  input: "./index.ts",
+  // preserveEntrySignatures: false,
+  // treeshake: true,
+  output: {
+    file: "build/bc.js",
+    format: "cjs",
+  },
+  plugins: [
+    typescript({
+      module: "ESNext",
+      moduleResolution: "node",
+      compilerOptions: {
+        paths: {
+          "csv-writer": ["node_modules/csv-writer/dist/index.js"], // point csv writer to bundled version
+        },
+      },
+    }),
+    nodeResolve({
+      preferBuiltins: true,
+    }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    json(),
+  ],
+};
