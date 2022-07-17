9b70d468afb53e6db480b8801d62bc4fd746d43b
kalouo <23177052+kalouo@users.noreply.github.com>
2022-07-17 21:45:45 +0200

    refactor request 1

--- src/plugin/notification/index.ts
+++ src/plugin/notification/index.ts
@@ -1,9 +1,14 @@
+import * as Discord from "./discord";
+import * as Telegram from "./telegram";
+
 import {
   ENotificationPluginKind,
   NotificationPlugin,
   NotificationPluginConfiguration,
 } from "./interfaces";
+
 import { name, version } from "../../../package.json";
+
 import { TelegramPluginConfiguration } from "./telegram/interfaces";
 const HOST_INFO = {
   id: name,
@@ -15,12 +20,14 @@ export const loadNotificationPlugin = async (
 ): Promise<NotificationPlugin> => {
   switch (config.type) {
     case ENotificationPluginKind.Discord:
-      return (await import("./discord")).getPlugin(config, HOST_INFO);
+      return await Discord.getPlugin(config, HOST_INFO);
     case ENotificationPluginKind.Telegram:
-      return await (
-        await import("./telegram")
-      ).getPlugin(config as TelegramPluginConfiguration);
+      return await Telegram.getPlugin(config as TelegramPluginConfiguration);
     default:
-      throw new Error(`Plugin ${config.type} not supported!`);
+      try {
+        return await (await import(config.type)).getPlugin(config);
+      } catch {
+        throw new Error(`Plugin ${config.type} not supported!`);
+      }
   }
 };
