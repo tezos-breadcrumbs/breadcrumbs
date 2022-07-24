95418c5866905baef6772d600d09a38e5db90a3c
cryi <cryi@tutanota.com>
2022-07-24 10:54:34 +0200

    exit codes

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import { run } from "src/cli";
 process.on("unhandledRejection", (reason) => {
   if (reason instanceof Error) {
     console.error(reason.message);
-    return;
+    process.exit(1);
   }
   console.error(reason);
+  process.exit(1);
 });
 
 run();
