2c2af5ec14f48b998f5c0771d83630aefb102816
cryi <cryi@tutanota.com>
2022-07-28 18:35:44 +0200

    support for ledger derivation paths

--- src/tezos-client/signers/ledger.ts
+++ src/tezos-client/signers/ledger.ts
@@ -1,10 +1,49 @@
 import TransportWebHID from "@ledgerhq/hw-transport-node-hid";
 import { LedgerSigner } from "@taquito/ledger-signer";
+import { readFile } from "fs/promises";
+import { LEDGER_SIGNER_CONFIG_FILE } from "src/utils/constants";
+import { parse } from "hjson";
+import { get } from "lodash";
+
+export const loadLedgerSignerConfig = async () => {
+  const config = {
+    derivation_path: `44'/1729'/0'/0'`,
+    /*
+      ED25519 = 0,
+      SECP256K1 = 1,
+      P256 = 2
+     */
+    derivation_type: 0,
+  };
+  try {
+    Object.assign(
+      config,
+      parse((await readFile(LEDGER_SIGNER_CONFIG_FILE)).toString())
+    );
+  } catch (err) {
+    if (get(err, "code") !== "ENOENT") {
+      console.log(
+        `Failed to load ledger configuration from ${LEDGER_SIGNER_CONFIG_FILE} - ${get(
+          err,
+          "message",
+          err
+        )}`
+      );
+    }
+  }
+  return config;
+};
 
 export const getLedgerSigner = async () => {
   console.log(`Please confirm public key export on your ledger:`);
+  const config = await loadLedgerSignerConfig();
   const transport = await TransportWebHID.create();
-  const ledgerSigner = new LedgerSigner(transport);
+  const ledgerSigner = new LedgerSigner(
+    transport,
+    config.derivation_path,
+    undefined,
+    config.derivation_type
+  );
   await ledgerSigner.publicKey();
   console.log(`Ledger connected.`);
   return ledgerSigner;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,7 @@ export const MUTEZ_FACTOR = 1000000;
 export const CONFIG_FILE = `config.hjson`;
 export const WALLET_PRIVATE_KEY_FILE = `payout_wallet_private.key`;
 export const REMOTE_SIGNER_CONFIG_FILE = `remote-signer.hjson`;
+export const LEDGER_SIGNER_CONFIG_FILE = `ledger-signer.hjson`;
 // reports
 export const REPORTS_DIRECTORY = "reports";
 export const REPORTS_SUCCESS_PAYMENTS_DIRECTORY = join(
