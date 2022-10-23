e49e67827942377dbf651da37c200b4dfe1f9426
kalouo <23177052+kalouo@users.noreply.github.com>
2022-10-23 13:26:20 +0200

    print donation payments

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -14,6 +14,7 @@ import {
   resolveExcludedPaymentsByContext,
   resolveExcludeDistributed,
   resolveSufficientBalance,
+  resolveDonations,
 } from "./steps";
 
 const steps: StepFunction[] = [
@@ -25,6 +26,7 @@ const steps: StepFunction[] = [
   resolveExcludedPaymentsByMinimumAmount,
   resolveExcludedPaymentsByMinimumDelegatorBalance,
   resolveSubstractTransactionFees,
+  resolveDonations,
   resolveFeeIncomeDistribution,
   resolveBondRewardDistribution,
   resolveExcludeDistributed,
